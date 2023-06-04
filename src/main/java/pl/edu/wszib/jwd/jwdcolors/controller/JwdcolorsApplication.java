f0008a2fd612a9a1604db6bb90b537efd01ba6a2
Jacek <jckkupczyk@gmail.com>
2023-06-04 11:07:08 +0200

    Konfiguracja dostępu do bazy danych

---
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/JwdcolorsApplication.java
@@ -0,0 +1,7 @@
+package pl.edu.wszib.jwd.jwdcolors.controller;
+
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("classpath:db.properties")
+public class JwdcolorsApplication {
+}
